2d40e30c741b62f5012ecb0cc517e6a29a647ccd
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-06-30 00:04:16 +0200

    SWIG: add python3 bindings
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

---
+++ swig/python3/examples/context.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+__author__ = "Mislav Novakovic <mislav.novakovic@sartura.hr>"
+__copyright__ = "Copyright 2017, Deutsche Telekom AG"
+__license__ = "Apache 2.0"
+
+import libyang_python3 as ly
+import sys
+
+try:
+	ctx = ly.Context("/etc/sysrepo2/yang")
+except Exception as e:
+	print(e)
+	err = ly.Error()
+	print("err: %d" % err.err())
+	print("vecode: %d" % err.vecode())
+	print("errmsg: "+err.errmsg())
+	print("errpath:"+err.errpath())
+	print("errapptag:"+err.errapptag())
+
+try:
+	ctx = ly.Context("/etc/sysrepo/yang")
+except Exception as e:
+	print(e)
+
+print("searchdir: " + ctx.get_searchdir())
+
+module = ctx.get_module("ietf-interfaces", None)
+if module is not None:
+	print(module.name())
+else:
+	module = ctx.load_module("ietf-interfaces", None)
+	if module is not None:
+		print(module.name())
---
+++ swig/python3/libyang_python3.i
@@ -0,0 +1,3 @@
+%module libyang_python3
+
+%include "../swig_base/python_base.i"
