78d63bc12c7185e9e9239c6382e59d392a8ddf2f
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-07-10 14:11:36 +0200

    add class Ext_Instance
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- swig/cpp/src/Tree_Schema.cpp
+++ swig/cpp/src/Tree_Schema.cpp
@@ -39,13 +39,33 @@ Module::Module(struct lys_module *module, S_Deleter deleter) {
 	_deleter = deleter;
 };
 Module::~Module() {};
+S_Revision Module::rev() {return _module->rev ? S_Revision(new Revision(_module->rev, _deleter)) : NULL;};
 
 Submodule::Submodule(struct lys_submodule *submodule, S_Deleter deleter) {
 	_submodule = submodule;
 	_deleter = deleter;
 };
 Submodule::~Submodule() {};
+S_Revision Submodule::rev() {return _submodule->rev ? S_Revision(new Revision(_submodule->rev, _deleter)) : NULL;};
 
+Ext_Instance::Ext_Instance(lys_ext_instance *ext_instance, S_Deleter deleter) {
+	_ext_instance = ext_instance;
+	_deleter = deleter;
+};
+Ext_Instance::~Ext_Instance() {};
+std::vector<S_Ext_Instance> *Ext_Instance::ext() {
+	auto s_vector = new vector<S_Ext_Instance>;
+
+	if (NULL == s_vector) {
+		return NULL;
+	}
+
+	for(uint8_t i = 0; i < _ext_instance->ext_size; i++) {
+		s_vector->push_back(S_Ext_Instance(new Ext_Instance(_ext_instance->ext[i], _deleter)));
+	}
+
+	return s_vector;
+}
 
 Revision::Revision(lys_revision *revision, S_Deleter deleter) {
 	_revisinon = revision;
