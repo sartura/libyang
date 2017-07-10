a02b7bbe8e5c51f80d1827231320bc1c9e77b6c1
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-07-10 11:17:58 +0200

    update example
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- swig/python3/examples/context.py
+++ swig/python3/examples/context.py
@@ -24,10 +24,8 @@ except Exception as e:
 	print(e)
 
 folders = ctx.get_searchdirs()
-print("\n",*folders, sep='\n')
-ctx.set_searchdir("/tmp")
-folders = ctx.get_searchdirs()
-print("\n",*folders, sep='\n')
+print(*folders, sep='\n')
+print("\n")
 
 module = ctx.get_module("ietf-interfaces", None)
 if module is not None:
@@ -37,11 +35,6 @@ else:
 	if module is not None:
 		print(module.name())
 
-
 modules = ctx.get_module_iter()
-if modules is None:
-    print("empty")
-else:
-    print(len(modules))
-    print(*modules, sep='\n')
-    print(modules[0].name)
+for module in modules:
+    print("module: " + module.name() + " prefix: " + module.prefix() + " type: " + str(module.type()))
